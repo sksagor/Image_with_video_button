4e5b33a5a583722bcde73a778d89dffb900e7b42
sksagor <sksagor007@gmail.com>
2021-03-24 14:16:49 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+        $("video-play-btn").magnificPopup({
+            type:'video',
+        });
+        
+    });
+    
+    jQuery(window).load(function(){
+        
+    });
+}(jQuery));	
